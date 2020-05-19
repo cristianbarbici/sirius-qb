2b60478b0984312be2befddcc9d5dc38af8b2a5c
Martin Hellspong <martin.hellspong@factor10.com>
2020-05-19 22:30:10 +0200

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './factor10-logo.svg';
+import logo from './sirius.svg';
 import './App.css';
 import SplProcess from "./SplatComponents/SplProcess";
 import CreateQuickBusiness from "./Components/CreateQuickBusiness";
@@ -9,10 +9,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <SplProcess name="ContractPOCoverview">
-          <CreateQuickBusiness />
-        </SplProcess>
       </header>
+      <SplProcess name="ContractPOCoverview">
+        <CreateQuickBusiness />
+      </SplProcess>
     </div>
   );
 }
