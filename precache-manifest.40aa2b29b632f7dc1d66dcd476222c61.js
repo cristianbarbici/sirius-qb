c284d3273c7e773643f5658f7d9b5d3aece2b49e
Cristian Barbici <cristian.barbici@siriusgroup.com>
2020-07-02 18:02:14 +0200

    Updates

---
+++ precache-manifest.40aa2b29b632f7dc1d66dcd476222c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc2cb25e9ded74cf46390f178ab4a53",
+    "url": "/sirius-qb/index.html"
+  },
+  {
+    "revision": "2559545f45652aa52fb9",
+    "url": "/sirius-qb/static/js/2.205d52cd.chunk.js"
+  },
+  {
+    "revision": "d683f7fda08bbbfccf5e70b48924783e",
+    "url": "/sirius-qb/static/js/2.205d52cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7f4f6f7a3ae3f9d276",
+    "url": "/sirius-qb/static/js/main.68dc9b73.chunk.js"
+  },
+  {
+    "revision": "288815bdd18a77163be5",
+    "url": "/sirius-qb/static/js/runtime-main.51356a88.js"
+  }
+]);
